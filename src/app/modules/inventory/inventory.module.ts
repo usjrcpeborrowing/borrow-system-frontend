e0a3071b73fb4e5b6f0ae21d71bae6876c267fa2
Neil Vincent Donaire <84239783+swift816@users.noreply.github.com>
2024-01-31 09:32:11 +0800

    Added header module and category components

--- src/app/modules/inventory/inventory.module.ts
+++ src/app/modules/inventory/inventory.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { HeaderModule } from '../header/header.module';
+
+import { CategoryComponent } from './category/category.component';
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { InventoryComponent } from './inventory/inventory.component';
 
-
 @NgModule({
   declarations: [
-    InventoryComponent
+    InventoryComponent,
+    CategoryComponent
   ],
   imports: [
     CommonModule,
-    InventoryRoutingModule
+    InventoryRoutingModule,
+    HeaderModule
   ]
 })
 export class InventoryModule { }
