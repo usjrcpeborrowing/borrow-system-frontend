e402a9da3bf57e6a4306ac9d397ab399fd258f31
Neil Vincent Donaire <84239783+swift816@users.noreply.github.com>
2024-03-05 11:20:00 +0800

    Changed the landing page

--- src/app/modules/landing-page/landing-page/landing-page.component.ts
+++ src/app/modules/landing-page/landing-page/landing-page.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
+  animations: [
+    trigger('panelTransition', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('200ms', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('200ms', style({ transform: 'translateX(100%)' }))
+      ])
+    ]),
+    trigger('logoTransition', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
+        animate('200ms', style({ transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        animate('200ms', style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class LandingPageComponent implements OnInit{
-  constructor(private router: Router) { }
+  constructor(private router: Router, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     
@@ -15,4 +35,7 @@ export class LandingPageComponent implements OnInit{
   directToLogin(): void {
     this.router.navigate(['/login']);
   }
+  
+
 }
+
--- src/app/modules/landing-page/page-panel/page-panel.component.ts
+++ src/app/modules/landing-page/page-panel/page-panel.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-page-panel',
   templateUrl: './page-panel.component.html',
   styleUrls: ['./page-panel.component.css']
 })
-export class PagePanelComponent {
+export class PagePanelComponent implements OnInit{
+  constructor(private router: Router, private renderer: Renderer2) { }
 
+  ngOnInit(): void {
+    
+  }
+  directToLogin(): void {
+    this.router.navigate(['/login']);
+  }
 }
