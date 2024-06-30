dde1c2e6688e1aed7f704a1709045378ac1c221e
usjrcpeborrowing <cpeborrowing@gmail.com>
2024-06-30 11:24:08 +0800

    update on authservice

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,11 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { EquipmentService } from 'src/app/services/equipment.service';
+import { environment } from 'src/environments/environment';
+
 interface User {
   userId: string;
   password: string;
@@ -32,137 +35,14 @@ export class AuthService {
   private static studentAccountId = 'student';
   private static studenPasswrd = 'student123';
   // add more accounts
-  constructor(private router: Router, private equipmentService: EquipmentService) {}
+  constructor(private router: Router, private equipmentService: EquipmentService, private http: HttpClient) {}
 
-  login(accountId: string, password: string): Observable<boolean> {
-    if (accountId === AuthService.adminAccountId && password === AuthService.adminPassword) {
-      const adminUser: User = {
-        userId: accountId,
-        password: password,
-        name: {
-          firstName: 'Admin',
-          lastName: 'Admin',
-        },
-        department: 'ECL',
-        role: 'Admin',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY2hvb2xJZCI6eyJzY2hvb2xJZCI6IjIwMjQyMjIyMjIifSwiaWF0IjoxNzE5MjM2NzMzLCJleHAiOjE3MjAxMDA3MzN9.XThBKRUgihYMO8WofyS1wDpbkLjdRrxr-sktnoRNdjo',
-      };
-      localStorage.setItem('currentUser', JSON.stringify(adminUser));
-      localStorage.setItem('token', adminUser.token);
-      this.navigateToDashboard(adminUser.role);
-      return of(true);
-    }
-
-    if (accountId === AuthService.readsAccountId && password === AuthService.readsPassword) {
-      const userAcc: User = {
-        userId: accountId,
-        password: password,
-        name: {
-          firstName: 'reads',
-          lastName: 'reads',
-        },
-        department: 'ECL',
-        role: 'reads',
-        token: '',
-      };
-      console.log(userAcc.name.firstName);
-      localStorage.setItem('currentUser', JSON.stringify(userAcc));
-      this.navigateToDashboard(userAcc.role);
-      return of(true);
-    }
-    if (accountId === AuthService.readscpeAccountId && password === AuthService.readscpePassword) {
-      const userAcc: User = {
-        userId: accountId,
-        password: password,
-        name: {
-          firstName: 'readscpe',
-          lastName: 'readscpe',
-        },
-        department: 'CPE',
-        role: 'reads',
-        token: '',
-      };
-      console.log(userAcc.name.firstName);
-      localStorage.setItem('currentUser', JSON.stringify(userAcc));
-      this.navigateToDashboard(userAcc.role);
-      return of(true);
-    }
-    if (accountId === AuthService.studentAccountId && password === AuthService.studenPasswrd) {
-      const userAcc: User = {
-        userId: accountId,
-        password: password,
-        name: {
-          firstName: 'Student',
-          lastName: 'Student',
-        },
-        department: 'ECL',
-        role: 'Student',
-        token: '',
-      };
-      localStorage.setItem('currentUser', JSON.stringify(userAcc));
-      this.navigateToDashboard(userAcc.role);
-      return of(true);
-    }
-    if (accountId === AuthService.oicAccountId && password === AuthService.oicPassword) {
-      const userAcc: User = {
-        userId: accountId,
-        password: password,
-        name: {
-          firstName: 'oic',
-          lastName: 'account',
-        },
-        department: 'ECL',
-        role: 'oic',
-        token: '',
-      };
-      localStorage.setItem('currentUser', JSON.stringify(userAcc));
-      this.navigateToDashboard(userAcc.role);
-      return of(true);
-    }
-    if (accountId === AuthService.instructorAccountId && password === AuthService.instructorPassword) {
-      const userAcc: User = {
-        userId: accountId,
-        password: password,
-        name: {
-          firstName: 'instructor',
-          lastName: 'account',
-        },
-        department: 'ECL',
-        role: 'Instructor',
-        token: '',
-      };
-      localStorage.setItem('currentUser', JSON.stringify(userAcc));
-      this.navigateToDashboard(userAcc.role);
-      return of(true);
-    }
-    return this.equipmentService.getUsers().pipe(
-      switchMap((response) => {
-        const users = response.data;
-        console.log('All users:', users);
-        const user = users.find((u: User) => u.userId === accountId);
-        if (user) {
-          return this.fetchUserType(user.role).pipe(
-            map((role) => {
-              user.role = role;
-              console.log('Identified role:', role);
-              localStorage.setItem('currentUser', JSON.stringify(user));
-              this.navigateToDashboard(user.role);
-              return true;
-            }),
-            catchError((error) => {
-              console.error('Failed to fetch user type', error);
-              return of(false);
-            })
-          );
-        } else {
-          return of(false);
-        }
-      }),
-      catchError((error) => {
-        console.error('Login failed', error);
-        return throwError(() => new Error('Login failed'));
-      })
-    );
+  login(accountId: string, password: string): Observable<Object> {
+    const body = {
+      schoolId: accountId,
+      password: password,
+    };
+    return this.http.post(environment.API_URL + '/api/inventoryreport', body).pipe(catchError(this.handleError));
   }
 
   private fetchUserType(userId: string): Observable<string> {
@@ -220,5 +100,9 @@ export class AuthService {
   isLoggedIn(): boolean {
     // return localStorage.getItem('currentUser') !== null;
     return localStorage.getItem('token') !== null;
+  }
+
+  handleError(err: HttpErrorResponse) {
+    return throwError(() => new Error(err.message));
   }
 }
