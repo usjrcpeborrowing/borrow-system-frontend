0a646e07b545e7508e227118a4cf229936361f9f
usjrcpeborrowing <cpeborrowing@gmail.com>
2024-07-14 22:59:43 +0800

    initial commit on socket io

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SnackbarComponent } from './snackbar/snackbar.component';
 import { MaterialModule } from '../material.module';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
-  declarations: [SnackbarComponent],
+  declarations: [SnackbarComponent, NotificationComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [SnackbarComponent],
+  exports: [SnackbarComponent, NotificationComponent],
 })
 export class SharedModule {}
