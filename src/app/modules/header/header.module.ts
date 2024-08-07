a652bdef7ea22b18eed93a59803730fc95ae7c2e
Neil Vincent Donaire <84239783+swift816@users.noreply.github.com>
2024-08-07 20:31:40 +0800

    headers

--- src/app/modules/header/header.module.ts
+++ src/app/modules/header/header.module.ts
@@ -8,6 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material.module';
 @NgModule({
   declarations: [
     HeaderComponent
@@ -18,7 +20,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule
+    MaterialModule,
+    MatMenuModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
--- src/app/modules/header/header/header.component.ts
+++ src/app/modules/header/header/header.component.ts
@@ -1,38 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { SocketioService } from 'src/app/services/socketio.service';
+
+interface NavigationItem {
+  name: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   currentUser: any;
-
-  constructor(private authService: AuthService, private router: Router,) { }
+  notification_count: number = 0;
+  notification_messages: string[] = [];
+  navigations: { [key: string]: NavigationItem[] } = {
+    administrator: [
+      { name: 'Dashboard', url: '/dashboard/admin' },
+      { name: 'Inventory', url: '/inventory' },
+      { name: 'Requests', url: '/faculty-borrowed-list' },
+      { name: 'Accounts', url: '/account-request' },
+      { name: 'History', url: '/history/admin' },
+    ],
+    oic: [
+      { name: 'Dashboard', url: '/dashboard/oic' },
+      { name: 'Inventory', url: '/inventory' },
+      { name: 'Requests', url: '/faculty-borrowed-list' },
+      { name: 'Accounts', url: '/account-request' },
+      { name: 'History', url: '/history/oic' },
+    ],
+    faculty: [
+      { name: 'Dashboard', url: '/dashboard/faculty' },
+      { name: 'Browse Items', url: '/borrow' },
+      { name: 'Requests', url: '/faculty-borrowed-list' },
+      { name: 'History', url: '/history/faculty' },
+    ],
+    student: [
+      { name: 'Dashboard', url: '/dashboard/student' },
+      { name: 'Browse Items', url: '/borrow' },
+      { name: 'Requests', url: '/faculty-borrowed-list' },
+      { name: 'History', url: '/history/student' },
+    ],
+  };
+  currentNavigations: any[] = [];
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private socketIOService: SocketioService
+  ) {}
 
   ngOnInit(): void {
     this.currentUser = this.authService.getCurrentUser();
+    this.updateNavigations();
+
+    this.socketIOService.listen('notification').subscribe({
+      next: (resp) => {
+        this.notification_count += 1;
+        this.notification_messages.push(resp as string);
+      },
+    });
   }
+
+  updateNavigations(): void {
+    const role = this.currentUser?.role;
+    if (role && this.navigations[role]) {
+      this.currentNavigations = this.navigations[role];
+    } else {
+      this.currentNavigations = [];
+    }
+  }
+
   logout(event: Event): void {
     event.preventDefault();
     this.authService.logout();
-  }
-  dashboard(event: Event): void {
-    
-    event.preventDefault();
-
-    let dashboardRoute = '/dashboard/instructor';
-    
-    this.router.navigate([dashboardRoute]);
-  }
-  inventory(event: Event): void {
-    event.preventDefault();
-    
-    this.router.navigate(['/borrow']);
-  }
-  history(event: Event): void {
-    event.preventDefault();
-    this.router.navigate(['/history/faculty']);
+    this.router.navigate(['/login']);
   }
 }
