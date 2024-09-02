c3e6949dc97ddd5ebe632a8460e92d4f77e5b4b3
Neil Vincent Donaire <84239783+swift816@users.noreply.github.com>
2024-09-02 23:05:13 +0800

    content tabs

--- src/app/modules/dashboard/content-tabs/content-tabs.component.ts
+++ src/app/modules/dashboard/content-tabs/content-tabs.component.ts
@@ -1,20 +1,74 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { BorrowedItemsService } from 'src/app/services/borrowed-item.services';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { InventoryFilter } from 'src/app/models/InventoryFilter';
+import { Pagination } from 'src/app/models/Pagination';
+import { EquipmentService } from 'src/app/services/equipment.service';
 
 @Component({
   selector: 'app-content-tabs',
   templateUrl: './content-tabs.component.html',
   styleUrls: ['./content-tabs.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
-export class ContentTabsComponent {
-  
-  borrowedItems: any[] = [];
-  constructor(private borrowedItemsService: BorrowedItemsService) { }
+export class ContentTabsComponent implements OnInit {
+  isFetching: boolean = false;
+  noItems: boolean = false;
+  itemlist: any[] = [];
+  pagination: Pagination = {
+    length: 0,
+    page: 1,
+    limit: 25,
+    pageSizeOption: [5, 10, 25, 50],
+  };
+  inventoryFilter: InventoryFilter = {
+    equipmenttype: '',
+    brand: '',
+    mattertype: '',
+    inventorytype: '',
+    description: '',
+    remarks: '',
+    department: '',
+    name: '',
+    dateAcquired: '',
+    location: '',
+  };
 
-  ngOnInit() {
-  this.borrowedItemsService.currentBorrowedItems.subscribe(items => {
-      this.borrowedItems = items;
-  });
+  constructor(
+    private equipmentService: EquipmentService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      this.queryParamsHandling(params);
+    });
+  }
+
+  queryParamsHandling(params: Params) {
+    this.inventoryFilter.name = params['search'] ? params['search'] : '';
+    this.pagination.page = params['page'] ? +params['page'] : 1;
+    this.getItems();
+  }
+
+  getItems() {
+    this.isFetching = true;
+    this.equipmentService.getAvailableEquipment(this.pagination, this.inventoryFilter).subscribe({
+      next: (resp) => {
+        this.isFetching = false;
+        this.itemlist = resp.data;
+        this.pagination.length = resp.total;
+        this.noItems = this.itemlist.length === 0;
+      },
+      error: (err) => {
+        this.isFetching = false;
+        this.noItems = true;
+        console.error('Error fetching items:', err);
+      },
+    });
+  }
+
+  onPageChange(event: any): void {
+    this.pagination.page = event.pageIndex + 1;
+    this.pagination.limit = event.pageSize;
+    this.getItems();
   }
 }
