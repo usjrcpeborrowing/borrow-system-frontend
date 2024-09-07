aa61282c2077983aa8773e0567f6fa7b46e80cd9
Neil Vincent Donaire <84239783+swift816@users.noreply.github.com>
2024-09-07 09:13:06 +0800

    added component

---
+++ src/app/modules/shared/dashboard-buttons/dashboard-buttons.component.ts
@@ -0,0 +1,52 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+@Component({
+  selector: 'app-dashboard-buttons',
+  templateUrl: './dashboard-buttons.component.html',
+  styleUrls: ['./dashboard-buttons.component.css']
+})
+export class DashboardButtonsComponent  implements OnInit{
+  
+  isSidebarExpanded = false;
+  currentUser: any;
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    // this.currentUser = this.authService.getCurrentUser();
+    // if (!this.currentUser || this.currentUser.role !== 'Student') {
+    //   this.router.navigate(['/']);
+    // }
+  }
+
+
+  toggleSidebar() {
+      this.isSidebarExpanded = !this.isSidebarExpanded;
+  }
+  directToBorrow(): void {
+    this.router.navigate(['/borrow']);
+  }
+  
+  directToInventory(): void {
+    this.router.navigate(['/inventory']);
+  }
+  directToItemDetails(): void {
+    this.router.navigate(['/item-details']);
+  }
+  
+  directToInventoryRequest(): void {
+    this.router.navigate(['/inventory-equipment-request']);
+  }
+  directToBorrowRequest(): void {
+    this.router.navigate(['/faculty-borrowed-list']);
+  }
+  
+  directToHistory(): void {
+    this.router.navigate(['/history/student']);
+  }
+  logout(event: Event): void {
+    event.preventDefault();
+    this.authService.logout();
+  }
+}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,16 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
 import { BorrowItemCategoryComponent } from './borrow-item-category/borrow-item-category.component';
+import { DashboardButtonsComponent } from './dashboard-buttons/dashboard-buttons.component';
 import { EquipmentDetailDialogComponent } from './equipment-detail-dialog/equipment-detail-dialog.component';
 import { NotificationComponent } from './notification/notification.component';
 import { SnackbarComponent } from './snackbar/snackbar.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { AdminHeaderModule } from '../admin-header/admin-header.module';
+import { HeaderOicModule } from '../header-oic/header-oic.module';
+import { HeaderReadsModule } from '../header-reads/header-reads.module';
+import { HeaderModule } from '../header/header.module';
+import { InventoryLevelsModule } from '../inventory-levels/inventory-levels.module';
+import { SystemReportsModule } from '../system-reports/system-reports.module';
 @NgModule({
-  declarations: [SnackbarComponent, NotificationComponent, BorrowItemCategoryComponent, EquipmentDetailDialogComponent],
-  imports: [CommonModule, MaterialModule, RouterModule, FormsModule, ReactiveFormsModule],
+  declarations: [SnackbarComponent, NotificationComponent, BorrowItemCategoryComponent, EquipmentDetailDialogComponent, DashboardButtonsComponent],
+  imports: [CommonModule, MaterialModule, RouterModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule,MatTableModule,
+    AdminHeaderModule,
+    HeaderOicModule,
+    HeaderReadsModule,
+    MatTabsModule,
+    HeaderModule,
+    InventoryLevelsModule,
+    SystemReportsModule
+  ],
   exports: [SnackbarComponent, NotificationComponent, BorrowItemCategoryComponent, EquipmentDetailDialogComponent],
 })
 export class SharedModule {}
