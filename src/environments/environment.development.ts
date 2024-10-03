5f5433c01869523e5cffb3a6f36dc70253f70f4c
cpeborrowing <cpeborrowing@gmail.com>
2024-10-03 18:20:16 +0800

    update favicon and API url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3000',
-  SOCKET_URI: 'ws://localhost:3000'
+  API_URL: 'https://assetmgt.engineering.usjr.edu.ph',
+  SOCKET_URI: 'https://assetmgt.engineering.usjr.edu.ph',
 };
