05efd7668ae6bdbf1a25ce665f456aa710a262bd
usjrcpeborrowing <cpeborrowing@gmail.com>
2024-10-16 00:02:22 +0800

    unavailable

--- src/app/modules/borrow/borrow-equipment-card/borrow-equipment-card.component.ts
+++ src/app/modules/borrow/borrow-equipment-card/borrow-equipment-card.component.ts
@@ -15,19 +15,22 @@ export class BorrowEquipmentCardComponent implements OnInit {
   defaultImage = '../../../../assets//equipment_default_image.png';
   displayImage: string = '';
   dialogWidth: string = '45%';
-  @Input() equipmentlist: Item | any;
+  available: string = 'available';
+  @Input() equipment: Item | any;
   @Input() item: Item = {} as Item | any;
   @Output() addToCart = new EventEmitter<Item>();
   constructor(public dialog: MatDialog, private _snackbar: MatSnackBar, private equipmentService: EquipmentService, private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit(): void {
-    const midsizeurl = this.equipmentlist?.images?.midSizeUrl?.length ? this.equipmentlist?.images?.midSizeUrl : '';
+    const midsizeurl = this.equipment?.images?.midSizeUrl?.length ? this.equipment?.images?.midSizeUrl : '';
     const id: string = midsizeurl.substring(midsizeurl.lastIndexOf('/d/') + 3, midsizeurl.lastIndexOf('/view'));
     // this.displayImage = `https://drive.google.com/thumbnail?id=${id}&&sz=w1000`;
 
     this.breakpointObserver.observe(['(max-width: 500px)']).subscribe((state: BreakpointState) => {
       this.dialogWidth = state.matches ? '100%' : '45%';
     });
+
+    this.available = this.equipment.availability == 'available' ? 'available' : 'unavailable';
   }
 
   addEquipment() {
@@ -36,7 +39,7 @@ export class BorrowEquipmentCardComponent implements OnInit {
     // if (!this.item.inventorytype || this.item.inventorytype === 'nonInventory') {
     //   this.item.quantity = 1;
     // }
-    this.addToCart.emit(this.equipmentlist);
+    this.addToCart.emit(this.equipment);
     this._snackbar.open('Item Added Successful', '', {
       horizontalPosition: 'center',
       verticalPosition: 'top',
@@ -47,7 +50,7 @@ export class BorrowEquipmentCardComponent implements OnInit {
 
   viewItemDetails(): void {
     this.dialog.open(EquipmentDetailComponent, {
-      data: this.equipmentlist,
+      data: this.equipment,
     });
   }
 }
