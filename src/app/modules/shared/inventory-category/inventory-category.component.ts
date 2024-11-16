48659e1de962d6a4696382824a973626e53eb45c
cpeborrowing <cpeborrowing@gmail.com>
2024-11-16 08:55:30 +0800

    added department in inventorycategory

--- src/app/modules/shared/inventory-category/inventory-category.component.ts
+++ src/app/modules/shared/inventory-category/inventory-category.component.ts
@@ -24,6 +24,7 @@ export class InventoryCategoryComponent implements OnInit, OnChanges {
   brands: string[] = [];
   matters: string[] = Constants.equipmentMatterType;
   inventorytypes: string[] = Constants.equipmentInventoryType;
+  departments: string[]=[]
 
   constructor(private fb: FormBuilder, private router: Router, private equipmentService: EquipmentService, private authService: AuthService) {
     this.filterForm = this.fb.group({
@@ -33,6 +34,7 @@ export class InventoryCategoryComponent implements OnInit, OnChanges {
       mattertype: [''],
       inventorytype: [''],
       location: [''],
+      department: [''],
       dateRange: this.fb.group({
         start: [''],
         end: [''],
@@ -46,6 +48,7 @@ export class InventoryCategoryComponent implements OnInit, OnChanges {
 
     this.user = this.authService.getCurrentUser() as User;
     this.url = this.router.url.split('?')[0];
+    this.departments = this.user.department;
   }
 
   ngOnInit(): void {
